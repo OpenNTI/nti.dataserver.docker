591295418b7a7a8729a1b8587ef96e6c78454a25
Jonathan Grimes <jsg2021@gmail.com>
2020-03-16 20:19:08 -0500

    use smaller images

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 export COMPOSE_DOCKER_CLI_BUILD=1 
 export DOCKER_BUILDKIT=1
 
-mkdir -p ./Library
-mkdir -p ./SiteAssets
+mkdir -p ./content/{sites,wiktionary}
+mkdir -p ./site-assets
 mkdir -p ./data
-mkdir -p ./var/caches/chameleon_cache/
-mkdir -p ./var/log/
+mkdir -p ./var/{caches,log,maildir}
+
+if [ ! -f ./content/wiktionary/dict.db ]; then
+    curl -L -o ./content/wiktionary/dict.tar.gz https://downloads.nextthought.com/wiktionary.tar.gz
+    tar -xf ./content/wiktionary/dict.tar.gz
+fi
 
 if [ ! -f ./.svnauth ]; then
     read -p "Subversion Username on https://repos.nextthought.com: " user
@@ -18,5 +22,10 @@ fi
 
 ssh-add -K ~/.ssh/id_rsa
 
-# docker-compose build
-docker build --no-cache --secret id=svnauth,src=./.svnauth --ssh default ./configs -t nti-dataserver
+docker image build \
+    --no-cache \
+    --secret id=svnauth,src=./.svnauth \
+    --ssh default \
+    ./configs \
+    -t nti-dataserver
+# --squash # still behind experimental flag
