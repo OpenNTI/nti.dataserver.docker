36958e7bd94186eac953755358a27f36481597ff
Jonathan Grimes <jsg2021@gmail.com>
2020-10-14 17:04:59 -0500

    Add solr service to docker

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e;
-export COMPOSE_DOCKER_CLI_BUILD=1 
+export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
 
 NOCACHE=""
 if [[ "$1" == "-force" ]]; then
     NOCACHE="--no-cache"
+	# see solr comment at the bottom as to why this needs `sudo`
+	sudo rm -rf ./var/solr
 fi
 
 mkdir -p ./content/{sites,wiktionary}
@@ -46,3 +48,21 @@ docker image build $NOCACHE \
     --ssh default \
     ./configs \
     -t nti-dataserver
+
+#setup host side of solr container
+if [ ! -d ./var/solr/nti ]; then
+	mkdir -p ./var/solr/nti
+	echo 'name=nti' > ./var/solr/nti/core.properties
+
+	TMP=`date "+%Y%m%d%k%M%s"`;
+	ID=`docker create -ti --name nti.temp-$TMP nti-dataserver:latest bash`
+	docker cp $ID:/code/sources/nti.solr/conf ./var/solr/nti/conf
+	docker rm -f $ID
+
+	# Solr's container makes solr run on their own normal user, it has an id of 8983.
+	# Docker's bind settings apply to the "owner"... which is the default user, and
+	# Solr changed it... and strangely docker echos the host's owner:group ids into
+	# the container... so instead of fixing this in the docker-compose/docker-command...
+	# we have to patch it here :(
+	sudo chown -R 8983:8983 ./var/solr
+fi
