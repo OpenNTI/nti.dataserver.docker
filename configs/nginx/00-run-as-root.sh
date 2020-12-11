e103fc17fbcfc3f384a2feb80d908f71f8790670
Jonathan Grimes <jsg2021@gmail.com>
2020-12-11 17:53:43 -0600

    Revert "make nginx maps work better on macOS?"
    This reverts commit 29ae33622b87d053ce5d79b98a236b8a422a915e.

--- configs/nginx/00-run-as-root.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-# macOS hosts have some quirks with filesystem ownership mapping. This side-steps it.
-# https://stackoverflow.com/questions/43097341/docker-on-macosx-does-not-translate-file-ownership-correctly-in-volumes3
-# since nginx runs as root BUT then drops to a non-privileged user, the osxfs miss-identifies who to say the mapped files belong to.
-sed -i -e 's/^\(user  \)nginx;/\1root;/' /etc/nginx/nginx.conf
