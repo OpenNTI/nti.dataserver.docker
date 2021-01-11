70b11dda4fd5f906313c94da213a5eda6bd9845e
ray hatfield <ray.hatfield@nextthought.com>
2021-01-11 12:42:06 -0600

    custom domain support (#3)
    * ddns cert setup
    * use standalone certbot option; remove nginx; add domain cli arg and usage info
    * update readme - custom domain setup
    * Update get-ddns-cert.sh to generate corresponding nginx config
    * .gitignore generated nginx configs

---
+++ configs/nginx/ddns/get-ddns-cert.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+set -e;
+
+scriptname=$0
+
+function usage {
+    echo "usage: $scriptname -d domain"
+    exit 1
+}
+
+while getopts ":d:h" opt; do
+  case ${opt} in
+    h) usage;;
+    d)
+      domain=$OPTARG
+    ;;
+    \?)
+      echo "Invalid Option: -$OPTARG" 1>&2
+      exit 1
+    ;;
+  esac
+done
+
+if [ -z $domain ]; then
+  usage
+fi
+
+
+cert_name="dev-cert"
+#cert_name="$domain"
+
+# script directory one-liner from https://stackoverflow.com/a/246128/636077
+base_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+data_path="$base_path/data/certbot"
+path="/etc/letsencrypt/live/$cert_name"
+mkdir -p "$data_path/conf/live/$cert_name"
+
+cat <<EOF >"$base_path/../conf.d/ddns-$domain.conf"
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+
+    server_name $domain;
+
+    ssl_certificate /etc/letsencrypt/live/dev-cert/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/dev-cert/privkey.pem;
+
+    include conf.d/server/root.conf;
+}
+EOF
+
+docker run \
+  -v $data_path/conf:/etc/letsencrypt:consistent,z \
+  -p 80:80 \
+  --name certbot \
+  --rm certbot/certbot \
+  certonly \
+    --standalone \
+    --cert-name $cert_name \
+    --register-unsafely-without-email \
+    -d $domain \
+    --rsa-key-size 4096 \
+    --agree-tos \
+    --no-eff-email \
+    --force-renewal
