fbe85ac8018a5db17009b931f38735d838b17dbb
Jonathan Grimes <jsg2021@gmail.com>
2021-03-02 12:02:57 -0600

    don't attempt to query docker after install if skipped

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e;
+
+if [ "$NTI_SKIP_DOCKER" != "" ]; then
+	exit 0;
+fi
+
+if [ ! command -v docker &> /dev/null ] || [ ! command -v docker-compose &> /dev/null ]; then
+	echo 'docker command line tools are not available';
+	exit 0;
+fi
+
 if [ ! -z `docker-compose ps -q nginx` ] && [ ! -z `docker ps -q --no-trunc | grep $(docker-compose ps -q nginx)` ]; then
   docker-compose restart nginx
 fi
